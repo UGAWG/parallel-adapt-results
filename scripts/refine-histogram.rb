566101a06d1a50615c23bc785908f01f80d58c6a
Mike Park <Mike.Park@NASA.gov>
2018-10-11 06:13:51 -0400

    run

--- scripts/refine-histogram.rb
+++ scripts/refine-histogram.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby
 
+def sh(comm)
+  puts comm
+  system comm
+end
+
 dirmeshbs = Dir['*/out.meshb'].sort
 dirmeshbs.each do |dirmeshb|
   dir = dirmeshb.split(/\//)[0]
   meshb = dirmeshb.split(/\//)[1]
   solb = meshb.sub(/\.meshb/,'-final-metric.solb')
-  puts "(cd #{dir} && ref_historgram #{meshb} #{solb}"
+  sh "(cd #{dir} && ref_historgram #{meshb} #{solb} )"
 end
