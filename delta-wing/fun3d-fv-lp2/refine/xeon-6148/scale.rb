4c89fcda1222e8ce9e8c6a2aa60445e194aee1d2
Mike Park <Mike.Park@NASA.Gov>
2018-10-12 10:25:08 -0400

    finner scale with zoltan

--- delta-wing/fun3d-fv-lp2/refine/xeon-6148/scale.rb
+++ delta-wing/fun3d-fv-lp2/refine/xeon-6148/scale.rb
@@ -6,11 +6,11 @@ def sh(comm)
 end
 
 cpn=40
-cores = [1,2,4,8,16,32,64,128,1*cpn,2*cpn,4*cpn,8*cpn]
+cores = [1,2,4,8,16,32,64,128,160,200,256,400]
 
 cores.each do |core|
   dir = sprintf("core%04d",core)
   sh "rm -rf #{dir}"
   sh "mkdir -p #{dir}"
-  sh "(cd #{dir} && pbswrap -e '. ~/.profile' -t 4 -q K4-route -cpn #{cpn} -np #{core} '~/refine/parmetis/src/ref_driver -i ../../../delta50k.meshb -m ../../../scaled-metric/delta500k-metric.solb -o out < /dev/null | tee refine_out')"
+  sh "(cd #{dir} && pbswrap -e '. ~/.profile' -t 4 -q K4-route -cpn #{cpn} -np #{core} '~/refine/zoltan/src/ref_driver -i ../../../delta50k.meshb -m ../../../scaled-metric/delta500k-metric.solb -o out < /dev/null | tee refine_out')"
 end
--- delta-wing/fun3d-fv-lp2/refine/xeon-x5675/scale.rb
+++ delta-wing/fun3d-fv-lp2/refine/xeon-x5675/scale.rb
@@ -6,11 +6,11 @@ def sh(comm)
 end
 
 cpn=12
-cores = [1,2,4,8,16,32,64,128,1*cpn,2*cpn,4*cpn,8*cpn]
+cores = [1,2,4,8,16,32,36,64,96,128,144,192,256,384,512,768]
 
 cores.each do |core|
   dir = sprintf("core%04d",core)
   sh "rm -rf #{dir}"
   sh "mkdir -p #{dir}"
-  sh "(cd #{dir} && pbswrap -e '. ~/.profile' -t 4 -q K2-route -cpn #{cpn} -np #{core} '~/refine/parmetis/src/ref_driver -i ../../../delta50k.meshb -m ../../../scaled-metric/delta500k-metric.solb -o out < /dev/null | tee refine_out')"
+  sh "(cd #{dir} && pbswrap -e '. ~/.profile' -t 4 -q K2-route -cpn #{cpn} -np #{core} '~/refine/zoltan/src/ref_driver -i ../../../delta50k.meshb -m ../../../scaled-metric/delta500k-metric.solb -o out < /dev/null | tee refine_out')"
 end
