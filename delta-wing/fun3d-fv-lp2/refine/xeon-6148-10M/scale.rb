b8076070d332ba2ef24631bfc9381aa0cfaf4903
Mike Park <Mike.Park@NASA.Gov>
2018-10-25 13:47:57 -0400

    try bigger case

---
+++ delta-wing/fun3d-fv-lp2/refine/xeon-6148-10M/scale.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+def sh(comm)
+  puts comm
+  system comm
+end
+
+cpn=40
+cores = [1,2,4,8,16,20,40,80,120,160,200,280,400]
+
+cores.each do |core|
+  dir = sprintf("core%04d",core)
+  sh "rm -rf #{dir}"
+  sh "mkdir -p #{dir}"
+  sh "(cd #{dir} && pbswrap -e '. ~/.profile' -t 12 -q K4-route -cpn #{cpn} -np #{core} '~/refine/zoltan/src/ref_driver -i ../../../delta50k.meshb -m ../../../scaled-metric/delta10M-metric.solb -o out < /dev/null | tee refine_out')"
+end
